898c2c51828d12e5be1690081abf39e629bccb84
DenisSvinkovskij <1911.denis55@gmail.com>
2020-10-05 11:21:48 +0300

    fixed task-06

--- task-06.js
+++ task-06.js
@@ -1,11 +1,16 @@
 const inputRef = document.querySelector('#validation-input');
 
 const onInputFocusChange = () => {
-  if (inputRef.value.length >= inputRef.dataset.length) {
+  if (
+    Number(inputRef.value.length) === Number(inputRef.dataset.length)
+  ) {
     inputRef.classList.add('valid');
     inputRef.classList.remove('invalid');
   }
-  if (inputRef.value.length < inputRef.dataset.length) {
+  if (
+    inputRef.value.length < inputRef.dataset.length ||
+    inputRef.value.length > inputRef.dataset.length
+  ) {
     inputRef.classList.add('invalid');
     inputRef.classList.remove('valid');
   }
